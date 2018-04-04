3aaaa120cc0d30a430683853edf359cc54c118a7
Martel Ramdin <martel1983@yahoo.com>
2018-04-04 18:39:17 -0500

    added some object, click events etc..

--- javascript.js
+++ javascript.js
@@ -1,28 +1,54 @@
-$(document).ready(function() {
- 
+$(document).ready(function () {
+
     
-    $(".jedi").on("click", function() {
-       $(".jedi").css("visibility", "hidden");
-       $("#jedi2").css("visibility", "visible")
-        
+    var button = $("#attack");
+
+    //selecting the player
+    $("#jedi1, #jedi, #sith, #sith1").on("click", function () {
+        $(this).css("border", "dashed 8px darkgreen");
+        $("#jedi1").css("visibility", "hidden").css("position", "absolute").css("top", "140px").css("right", "455px").css("background-color", "red").css("visibility", "visible");
+        $("#sith").css("visibility", "hidden").css("position", "absolute").css("top", "140px").css("right", "435px").css("background-color", "red").css("visibility", "visible");
+        $("#sith1").css("visibility", "hidden").css("position", "absolute").css("top", "140px").css("right", "425px").css("background-color", "red").css("visibility", "visible");
+        $(button).html("<button type='button'>ATTACK</button>");
     });
-
-    alert("hello");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 });
+
+var jedi = {
+    firstName: "Luke",
+    lastName: "Skywalker",
+    Health: 100,
+    Damage: 10,
+    location: $("#jedi"),
+    
+
+};
+var jedi1 = {
+    firstName: "Obi-Wan ",
+    lastName: "Kenobi",
+    Health: 100,
+    Damage: 10,
+    location: $("#jedi1"),
+   
+
+};
+var sith = {
+    firstName: "Darth",
+    lastName: "Maul",
+    Health: 100,
+    Damage: 10,
+    location: $("#sith")
+
+};
+var sith1 = {
+    firstName: "Darth",
+    lastName: "Vader",
+    Health: 100,
+    Damage: 10,
+    location: $("sith1"),
+
+};
+
+alert(jedi.firstName);
