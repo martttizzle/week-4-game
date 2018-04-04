d102d6f880306c07972b141ce359ed44ad69d1f8
Vinod <ramdinv@gmail.com>
2018-04-03 22:27:50 -0500

    first completion of movement

---
+++ javascript.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+ 
+    
+    $(".jedi").on("click", function() {
+       $(".jedi").css("visibility", "hidden");
+       $("#jedi2").css("visibility", "visible")
+        
+    });
+
+    alert("hello");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
