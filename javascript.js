428f12b56a33a33390ffb69c441fce15383d42ae
Vinod <ramdinv@gmail.com>
2018-04-06 07:12:52 -0500

    click

--- javascript.js
+++ javascript.js
@@ -1,19 +1,42 @@
 $(document).ready(function () {
+     var a;
+     
+     document.getElementById("jedi").addEventListener("click",  jediSelected) = a; 
+     document.getElementById("jedi1").addEventListener("click", jedi1Selected);
+     document.getElementById("sith").addEventListener("click",  sithSelected);
+     document.getElementById("sith1").addEventListener("click", sith1Selected);
 
-    
-    var button = $("#attack");
+     if(a){
+         alert("yea");
+     }
+   
 
-    //selecting the player
-    $("#jedi, #jedi1, #sith, #sith1").on("click", function () {
-        $(this).css("border", "dashed 8px darkgreen");
+    function jediSelected() {       
+    }
 
-        $("#jedi1").addClass("jedi1NC");
+    function jedi1Selected() {       
+         $("#jedi1").addClass("jedi1NC");
+        
+    }
+    function sithSelected() {       
+        
         $("#sith").addClass("jediNC");
-        $("#sith1").addClass("jedi2NC");
-        $(button).html("<button type='button'>ATTACK</button>");
+        
+    }
+    function sith1Selected() {       
+        
+         $("#sith1").addClass("jedi2NC");
+        
+     }
+ 
+    
+       
+       //.css("border", "dashed 8px darkgreen");
+       
+        // $(button).html("<button type='button'>ATTACK</button>");
     });
 
-});
+ 
 
 var jedi = {
     firstName: "Luke",
